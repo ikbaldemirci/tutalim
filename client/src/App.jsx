539a4ec430a0f59adc922c5525a54a39a76920f3
aydin-elif <elif6aydin@gmail.com>
2025-05-21 19:26:06 +0300

    patlamadan son hali kalsın bi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,64 @@
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Signup from "./Signup";
+// import Login from "./Login";
+// import OwnerHome from "./OwnerHome";
+// import RealtorHome from "./RealtorHome";
+// // import UserHome from "./UserHome";
+// import ProtectedRoute from "./ProtectedRoute";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/signup" element={<Signup />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route
+//           path="/owner"
+//           element={
+//             <ProtectedRoute role="ev sahibi">
+//               <OwnerHome />
+//             </ProtectedRoute>
+//           }
+//         />
+//         <Route
+//           path="/realtor"
+//           element={
+//             <ProtectedRoute role="emlakçı">
+//               <RealtorHome />
+//             </ProtectedRoute>
+//           }
+//         />
+//         {/* <Route
+//           path="/user"
+//           element={
+//             <ProtectedRoute role="kullanıcı">
+//               <UserHome />
+//             </ProtectedRoute>
+//           }
+//         /> */}
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+// export default App;
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
 import Login from "./Login";
-import OwnerHome from "./OwnerHome";
-import RealtorHome from "./RealtorHome";
-// import UserHome from "./UserHome";
-import ProtectedRoute from "./ProtectedRoute";
+import Home from "./Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/owner"
-          element={
-            <ProtectedRoute role="ev sahibi">
-              <OwnerHome />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/realtor"
-          element={
-            <ProtectedRoute role="emlakçı">
-              <RealtorHome />
-            </ProtectedRoute>
-          }
-        />
-        {/* <Route
-          path="/user"
-          element={
-            <ProtectedRoute role="kullanıcı">
-              <UserHome />
-            </ProtectedRoute>
-          }
-        /> */}
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
