575c3caf24189f16688b27a95b2badd2cf9610ef
ikbal <muhammed_5m@hotmail.com>
2025-06-13 14:17:41 +0300

    role based login orientation

--- client/src/pages/RealtorHome.jsx
+++ client/src/pages/RealtorHome.jsx
@@ -1,15 +1,27 @@
 import jwtDecode from "jwt-decode";
 import Navbar from "../components/Navbar";
+// import { Typography, Button, LogoutIcon } from "@mui/material";
 
 function RealtorHome() {
   const token = localStorage.getItem("token");
   const decoded = token ? jwtDecode(token) : null;
 
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/";
+  };
+
   return (
     <div>
       <Navbar />
-      <h1>Hoşgeldiniz {decoded?.name} - Emlakçı Paneli</h1>
-      <p>Burada ilanlarınızı yönetebilirsiniz.</p>
+      <Typography>Hoş geldiniz, {decoded?.name}!</Typography>
+      <Button
+        variant="contained"
+        endIcon={<LogoutIcon />}
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
     </div>
   );
 }
