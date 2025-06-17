4cae1accca4a6ca486cd19074f4db2ea8365a417
ikbal <muhammed_5m@hotmail.com>
2025-06-17 11:41:29 +0300

    basic table with api

--- client/src/pages/RealtorHome.jsx
+++ client/src/pages/RealtorHome.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 import Navbar from "../components/Navbar";
 import { Typography, Button } from "@mui/material";
@@ -8,6 +10,26 @@ function RealtorHome() {
   const token = localStorage.getItem("token");
   const decoded = token ? jwtDecode(token) : null;
 
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    if (token) {
+      const decoded = jwtDecode(token);
+      const realtorId = decoded.id;
+
+      axios
+        .get(`http://localhost:5000/api/properties?realtorId=${realtorId}`)
+        .then((res) => {
+          if (res.data.status === "success") {
+            setProperties(res.data.properties);
+          }
+        })
+        .catch((err) => {
+          console.error("Veri çekme hatası:", err);
+        });
+    }
+  }, [token]);
+
   const handleLogout = () => {
     localStorage.removeItem("token");
     window.location.href = "/";
@@ -17,7 +39,7 @@ function RealtorHome() {
     <div>
       <Navbar />
       <Typography>Hoş geldiniz, {decoded?.name}!</Typography>
-      <BasicTable />
+      <BasicTable data={properties} />
       <Button
         variant="contained"
         endIcon={<LogoutIcon />}
